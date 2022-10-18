b52588ebc3acb31ee67a9e21136406131031426f
m-11400988 <115515094+m-11400988@users.noreply.github.com>
2022-10-18 10:10:24 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,3 +1,17 @@
+if (input.isGesture(Gesture.Shake)) {
+    pins.analogWritePin(AnalogPin.P1, 1023)
+    basic.pause(100)
+    pins.digitalWritePin(DigitalPin.P0, 0)
+    basic.pause(100)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    basic.pause(100)
+    pins.analogWritePin(AnalogPin.P0, 1023)
+    basic.pause(100)
+}
 basic.forever(function () {
-	
+    if (input.lightLevel() < 80) {
+        pins.servoWritePin(AnalogPin.P2, 180)
+    } else {
+        pins.servoWritePin(AnalogPin.P2, 0)
+    }
 })
